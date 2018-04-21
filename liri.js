2d206f74d5e61686a7425c1c7d9892ab7e364ac8
Madinah2491 <madinah2491@gmail.com>
2018-04-21 10:08:23 -0400

    added omdb +

--- liri.js
+++ liri.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 var keys = require ("./keys.js");
 
+var search = process.argv[2];
 
 var Spotify = require('node-spotify-api');
 
@@ -20,10 +21,10 @@ var Spotify = require('node-spotify-api');
     });
  } 
 
- function Twitter (){
+var Twitter = require('twitter');
 
-    var Twitter = require('twitter');
- 
+ function mytweets (){
+
     var client = new Twitter({
     consumer_key: keys.twitter.consumer_key,
     consumer_secret: keys.twitter.consumer_secret,
@@ -32,12 +33,71 @@ var Spotify = require('node-spotify-api');
     });
     
     var params = {screen_name: 'yepitsmadinah'};
-    client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) {
-        console.log(tweets[0].text);
+    
+    client.get('search/tweets', params, function(error, tweets, response) {
+        
+        if (!error) {
+        console.log(tweets[1].text);
     }
     });
 
  }
 
- Twitter();
+ mytweets();
+ spotifySongs();
+
+ var request = require("request");
+
+// Store all of the arguments in an array
+var nodeArgs = process.argv;
+
+// Create an empty variable for holding the movie name
+var movieName = "";
+
+// Loop through all the words in the node argument
+// And do a little for-loop magic to handle the inclusion of "+"s
+for (var i = 2; i < nodeArgs.length; i++) {
+
+  if (i > 2 && i < nodeArgs.length) {
+
+    movieName = movieName + "+" + nodeArgs[i];
+
+  }
+
+  else {
+
+    movieName += nodeArgs[i];
+
+  }
+}
+
+// Then run a request to the OMDB API with the movie specified
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+// This line is just to help us debug against the actual URL.
+console.log(queryUrl);
+
+request(queryUrl, function(error, response, body) {
+
+  // If the request is successful
+  if (!error && response.statusCode === 200) {
+
+    // Parse the body of the site and recover just the imdbRating
+    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+    console.log("Release Year: " + JSON.parse(body).Year);
+  }
+});
+//  1. `node liri.js my-tweets`
+
+//    * This will show your last 20 tweets and when they were created at in your terminal/bash window.
+
+// 2. `node liri.js spotify-this-song '<song name here>'`
+
+//    * This will show the following information about the song in your terminal/bash window
+     
+//      * Artist(s)
+     
+//      * The song's name
+     
+//      * A preview link of the song from Spotify
+     
